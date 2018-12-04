83f1ece99f775a35eb1425ccf24a124f65d037ab
Alisson Pereira <lissonpsantos2@gmail.com>
2018-12-04 11:43:40 -0300

    cria a classe dos tokens

---
+++ code/Token.java
@@ -0,0 +1,52 @@
+public class Token {
+  public byte kind;
+  public String spelling;
+
+  public Token (byte kind, String spelling) {
+    this.kind = kind;
+    this.spelling = spelling;
+  }
+
+  public final static byte
+    ASSIGN = 0,
+    BEGIN = 1,
+    END = 2,
+    IF = 3,
+    THEN = 4,
+    ELSE = 5,
+    LPAREN = 6,
+    RPAREN = 7,
+    VAR = 8,
+    COLON = 9,
+    INTLIT = 10,
+    DOT = 11,
+    DOTDOT = 12,
+    SEMICOLON = 13,
+    FLOATLIT = 14,
+    ID = 15,
+    WHILE = 16,
+    DO = 17,
+    PLUS = 18,
+    MINUS = 19,
+    OR = 20,
+    TIMES = 21,
+    SLASH = 22,
+    AND = 23,
+    GREATERTHAN = 24,
+    LESSTHAN = 25,
+    GREATEREQUAL = 26,
+    LESSEQUAL = 27,
+    EQUAL = 28,
+    DIFFERENT = 29,
+    ERRORTOKEN = 30,
+    LBRACKET = 31,
+    RBRACKET = 32,
+    INTEGER = 33,
+    REAL = 34,
+    BOOLEAN = 35,
+    TRUE = 36,
+    FALSE = 37,
+    OF = 38,
+    ARRAY = 39,
+    COMMA = 8;
+}
