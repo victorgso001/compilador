ea8858e4ae48fd47ec50ba377fca0dee4db5349b
mauricio <mss.soares@hotmail.com>
2019-01-25 03:12:06 -0300

    read .txt file

--- Eclipse Project/src/analysers/Main.java
+++ Eclipse Project/src/analysers/Main.java
@@ -1,27 +1,39 @@
+/*
+ * 	PARA TESTAR, O CÓDIGO DEVE ESTAR ESCRITO NUM ARQUIVO COMO "d:\\code.txt" 
+ */
+
 package analysers;
 
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		Scanner ler = new Scanner(System.in);	// just to read data entry
-		String path;
 		
-		System.out.printf("Entre com o código:\n");
-		path = ler.nextLine();
+		String path = new String();
 		
-		FileWriter arq = new FileWriter("d:\\programa.txt");
-		PrintWriter gravarArq = new PrintWriter(arq);
+		try {
+			FileReader arq = new FileReader("d:\\code.txt");
+			BufferedReader lerArq = new BufferedReader(arq);
+			
+			String text = lerArq.readLine();			
+			path = text + "\n";
+			
+			while(text != null) {
+				text = lerArq.readLine();
+				path = path + text + "\n";
+			}
+			
+			lerArq.close();
+			
+		}catch (IOException e) {
+	        System.err.printf("Erro na abertura do arquivo: %s.\n",e.getMessage());
+	    }
 		
-		gravarArq.printf(path);
-		
-		arq.close();
-		
+		System.out.print(path);
 		
 	}
 
