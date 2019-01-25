5b22ab1f17d01366c48cf5ae05d5eb5210971d66
mauricio <mss.soares@hotmail.com>
2019-01-25 01:21:35 -0300

    coding main

--- Eclipse Project/src/analysers/Main.java
+++ Eclipse Project/src/analysers/Main.java
@@ -1,8 +1,28 @@
 package analysers;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		Scanner ler = new Scanner(System.in);	// just to read data entry
+		String path;
+		
+		System.out.printf("Entre com o código:\n");
+		path = ler.nextLine();
+		
+		FileWriter arq = new FileWriter("d:\\programa.txt");
+		PrintWriter gravarArq = new PrintWriter(arq);
+		
+		gravarArq.printf(path);
+		
+		arq.close();
+		
+		
 	}
 
 }
